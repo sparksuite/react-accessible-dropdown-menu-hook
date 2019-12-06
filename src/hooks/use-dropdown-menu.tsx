58ae7e9155d00ae092464cfea253beddbf9ad1f5
Cory Harper <cory@hoa-express.com>
2019-12-06 16:09:16 -0600

    added source code

---
+++ src/hooks/use-dropdown-menu.tsx
@@ -0,0 +1,144 @@
+// Imports
+import React, { useState, useRef, createRef } from 'react';
+import useUpdateOnlyEffect from './use-update-only-effect';
+
+
+// A custom Hook that abstracts away the listeners/controls for dropdown menus
+export default (itemCount: number) => {
+   // Use state
+   const [isOpen, setIsOpen] = useState<boolean>(false);
+   const [returnFocusToButton, setReturnFocusToButton] = useState<boolean>(false);
+   const [currentFocusIndex, setCurrentFocusIndex] = useState<number | null>(null);
+   
+   
+   // Create refs
+   const buttonRef = useRef<HTMLButtonElement>(null);
+   const itemRefs = useRef([...Array(itemCount)].map(() => createRef<HTMLAnchorElement>()));
+   
+   
+   // Create type guard
+   const isKeyboardEvent = (e: React.KeyboardEvent | React.MouseEvent): e is React.KeyboardEvent => (e as React.KeyboardEvent).key !== undefined;
+   
+   
+   // Handle changes in the state of the menu
+   useUpdateOnlyEffect(() => {
+       // Return if the ref currently points to a null value
+       if (!itemRefs.current[0].current || !buttonRef.current) {
+           return;
+       }
+       
+       
+       // If the menu is currently open focus on the first item in the menu, else return focus to the button if state indicates that is desired behavior
+       if (isOpen) {
+           itemRefs.current[0].current.focus();
+           setCurrentFocusIndex(0);
+       } else if (returnFocusToButton) {
+           buttonRef.current.focus();
+       }
+   }, [isOpen, returnFocusToButton]);
+   
+   
+   // Handle changes in the currently focused element
+   useUpdateOnlyEffect(() => {
+       if (currentFocusIndex !== null) {
+           itemRefs.current[currentFocusIndex].current!.focus(); // TODO: Optional chaining
+       }
+   }, [currentFocusIndex]);
+   
+   
+   // Create handler function for button click and keyboard event
+   const buttonListener = (e: React.KeyboardEvent | React.MouseEvent) => {
+       // Detect if event was a keyboard event or a mouse event
+       if (isKeyboardEvent(e)) {
+           const { key } = e;
+           
+           if (!(key === 'Tab' || key === 'Shift')) {
+               e.preventDefault();
+           }
+           if (key === 'Enter' || key === ' ') {
+               setIsOpen(true);
+               setReturnFocusToButton(false);
+           } else if (key === 'Tab') {
+               setIsOpen(false);
+               setReturnFocusToButton(false);
+           }
+       } else {
+           setIsOpen(!isOpen);
+           setReturnFocusToButton(true);
+       }
+   };
+   
+   
+   // Create handler function for menu controls via keyboard events
+   const itemListener = (e: React.KeyboardEvent<HTMLAnchorElement>) => {
+       // Create mutable value that initializes as the currentFocusIndex value
+       let newFocusIndex = currentFocusIndex;
+       
+       const { key } = e;
+       
+       
+       // Prevent default browser behavior except when desirable
+       if (!(key === 'Tab' || key === 'Shift' || key === 'Enter')) {
+           e.preventDefault();
+       }
+       
+       
+       // Create conditional behavior based based on which key was pressed
+       // Controls whether the menu is open or closed, if the button should regain focus on close, and if a handler function should be called
+       if (key === 'Escape') {
+           setIsOpen(false);
+           setReturnFocusToButton(true);
+           return;
+       } else if (key === 'Tab') {
+           setIsOpen(false);
+           setReturnFocusToButton(false);
+           return;
+       } else if (key === 'Enter') {
+           e.currentTarget.click();
+           return;
+       }
+       
+       
+       // Controls the current index to focus
+       if (newFocusIndex !== null) {
+           if (key === 'ArrowUp') {
+               newFocusIndex -= 1;
+           } else if (key === 'ArrowDown') {
+               newFocusIndex += 1;
+           }
+           
+           if (newFocusIndex > itemRefs.current.length - 1) {
+               newFocusIndex = 0;
+           } else if (newFocusIndex < 0) {
+               newFocusIndex = itemRefs.current.length - 1;
+           }
+       }
+       
+       
+       // After any modification set state to the modified value
+       setCurrentFocusIndex(newFocusIndex);
+   };
+   
+   
+   // Define spreadable props for button and items
+   const buttonProps: React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> = {
+       onKeyDown: buttonListener,
+       onClick: buttonListener,
+       tabIndex: 0,
+       ref: buttonRef,
+       role: 'button',
+       'aria-haspopup': true,
+       'aria-expanded': isOpen,
+   };
+   
+   const itemProps = [...Array(itemCount)].map((ignore, index) => ({
+       onKeyDown: itemListener,
+       tabIndex: -1,
+       role: 'menuitem',
+       ref: itemRefs.current[index],
+   }));
+   
+   
+   // Return a listener for the button, individual list items, and the state of the menu
+   return [buttonProps, itemProps, isOpen] as const;
+};
