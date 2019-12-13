0e3f8e56b641ad8b4c9f45208ff437bd52ee7834
Wes Cossick <wes@sparksuite.com>
2019-12-13 09:39:19 -0600

    Add very basic demo code based on test component

--- demo/src/app.tsx
+++ demo/src/app.tsx
@@ -1,10 +1,37 @@
 // Imports
 import React from 'react';
 import './app.css';
+import useDropdownMenu from 'react-accessible-dropdown-menu-hook';
 
 // Functional component
 const App: React.FC = () => {
-	return <div className='app'>Hello world!</div>;
+	const [buttonProps, itemProps, isOpen] = useDropdownMenu(3);
+
+	return (
+		<div className='app'>
+			<button type='button' {...buttonProps} id='menubutton' tabIndex={0}>
+				Primary
+			</button>
+
+			<div style={{ display: isOpen ? 'block' : 'none' }} role='menu' id='menu'>
+				<a style={{ display: 'block' }} {...itemProps[0]} onClick={() => null} id='menuitem1'>
+					Item 1
+				</a>
+
+				<a style={{ display: 'block' }} href='https://example.com' {...itemProps[1]} id='menuitem2'>
+					Item 2
+				</a>
+
+				<a style={{ display: 'block' }} href='https://example.com' {...itemProps[2]} id='menuitem3'>
+					Item 3
+				</a>
+			</div>
+
+			<button type='button' id='secondbutton' tabIndex={0}>
+				Another Button
+			</button>
+		</div>
+	);
 };
 
 export default App;
