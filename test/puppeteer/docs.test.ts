b8b48bffb85308e651f83f5bb479ce3158ae7131
Cory Harper <cory@hoa-express.com>
2019-12-16 14:15:13 -0600

    added dev server to puppeteer tests

--- test/puppeteer/docs.test.ts
+++ test/puppeteer/docs.test.ts
@@ -1,11 +1,23 @@
 // Imports
 import path from 'path';
 
+const { keyboard } = page;
+
 // Tests
 beforeAll(async () => {
-	await page.goto(`file://${path.join(__dirname, '..', '..', 'docs', 'build', 'index.html')}`);
+	await page.goto('http://localhost:3000');
+	await page.waitFor(() => document.querySelector("#menubutton"));
 });
 
 it('should have correct page title', async () => {
 	await expect(page.title()).resolves.toMatch('React Accessible Dropdown Menu Hook');
+});
+
+it('should have content', async () => {
+	await page.focus('#menubutton');
+	await keyboard.down('Enter');
+
+	const currentFocus = await page.evaluate(() => document.activeElement.id);
+
+	expect(currentFocus).toBe("menuitem1");
 });
