cee52f5e09211d62eff6ad71bd841c1d3cb8810a
Cory Harper <cory@hoa-express.com>
2019-12-16 15:52:01 -0600

    added preliminary tests

--- test/puppeteer/docs.test.ts
+++ test/puppeteer/docs.test.ts
@@ -1,21 +1,61 @@
 // Destructured constant for readability
 const { keyboard } = page;
 
+
+// Helper functions used in multiple tests
+const menuDisplayState = () => page.evaluate(() => document.querySelector<HTMLDivElement>("#menu").style.display); 
+const currentFocusId = () => page.evaluate(() => document.activeElement.id);
+
 // Tests
-beforeAll(async () => {
+beforeEach(async () => {
 	await page.goto('http://localhost:3000');
-	await page.waitFor(() => document.querySelector('#menubutton'));
+	await page.waitFor("#menubutton");
 });
 
-it('should have correct page title', async () => {
+it('has the correct page title', async () => {
 	await expect(page.title()).resolves.toMatch('React Accessible Dropdown Menu Hook');
 });
 
-it('should focus on the first menu item when the enter key is pressed', async () => {
+it('focuses on the first menu item when the enter key is pressed', async () => {
 	await page.focus('#menubutton');
 	await keyboard.down('Enter');
 
-	const currentFocus = await page.evaluate(() => document.activeElement.id);
+	expect(await currentFocusId()).toBe('menuitem1');
+});
 
-	expect(currentFocus).toBe('menuitem1');
+it('hides the menu by default and make it visible if it is open', async () => {
+	expect(await menuDisplayState()).toBe("none");
+
+	await page.focus('#menubutton');
+	await keyboard.down('Enter');
+
+	expect(await menuDisplayState()).toBe("block");
+});
+
+it('hides the menu if the escape key is pressed, and focuses on the menu button', async () => {
+	await page.focus('#menubutton');
+	await keyboard.down('Enter');
+
+	await keyboard.down('Escape');
+	expect(await menuDisplayState()).toBe('none');
+	expect(await currentFocusId()).toBe('menubutton');
+});
+
+it('hides the menu if the tab key is pressed, and focuses on the next item in the tab order', async () => {
+	await page.focus('#menubutton');
+	await keyboard.down('Enter');
+
+	await keyboard.down('Tab');
+	expect(await menuDisplayState()).toBe('none');
+	expect(await currentFocusId()).toBe('secondbutton');
+});
+
+it('hides the menu if the tab key is pressed while the shift key is held, and focuses on the previous item in the tab order', async () => {
+	await page.focus('#menubutton');
+	await keyboard.down('Enter');
+
+	await keyboard.down('Shift');
+	await keyboard.down('Tab');
+	expect(await menuDisplayState()).toBe('none');
+	expect(await currentFocusId()).toBe('menubutton');
 });
