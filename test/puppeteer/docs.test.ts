c25143dc7cad651b5bccf9a066b0bc90aec7e172
Cory Harper <cory@hoa-express.com>
2019-12-17 14:51:12 -0600

    removed display checks

--- test/puppeteer/docs.test.ts
+++ test/puppeteer/docs.test.ts
@@ -5,7 +5,6 @@ import path from 'path';
 const { keyboard } = page;
 
 // Helper functions used in multiple tests
-const menuDisplayState = () => page.evaluate(() => document.querySelector<HTMLDivElement>('#menu').style.display);
 const currentFocusID = () => page.evaluate(() => document.activeElement.id);
 
 // Tests
@@ -24,39 +23,27 @@ it('focuses on the first menu item when the enter key is pressed', async () => {
 	expect(await currentFocusID()).toBe('menuitem1');
 });
 
-it('hides the menu by default and make it visible if it is open', async () => {
-	expect(await menuDisplayState()).toBe('none');
-
-	await page.focus('#menubutton');
-	await keyboard.down('Enter');
-
-	expect(await menuDisplayState()).toBe('block');
-});
-
-it('hides the menu if the escape key is pressed, and focuses on the menu button', async () => {
+it('focuses on the menu button', async () => {
 	await page.focus('#menubutton');
 	await keyboard.down('Enter');
 
 	await keyboard.down('Escape');
-	expect(await menuDisplayState()).toBe('none');
 	expect(await currentFocusID()).toBe('menubutton');
 });
 
-it('hides the menu if the tab key is pressed, and focuses on the next item in the tab order', async () => {
+it('focuses on the next item in the tab order', async () => {
 	await page.focus('#menubutton');
 	await keyboard.down('Enter');
 
 	await keyboard.down('Tab');
-	expect(await menuDisplayState()).toBe('none');
 	expect(await currentFocusID()).toBe('secondbutton');
 });
 
-it('hides the menu if the tab key is pressed while the shift key is held, and focuses on the previous item in the tab order', async () => {
+it('focuses on the previous item in the tab order', async () => {
 	await page.focus('#menubutton');
 	await keyboard.down('Enter');
 
 	await keyboard.down('Shift');
 	await keyboard.down('Tab');
-	expect(await menuDisplayState()).toBe('none');
 	expect(await currentFocusID()).toBe('menubutton');
 });
