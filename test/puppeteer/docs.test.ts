7b2f33982833815bf9b7fe2493be466603d91b91
Cory Harper <cory@hoa-express.com>
2019-12-17 14:47:15 -0600

    resolved all comments

--- test/puppeteer/docs.test.ts
+++ test/puppeteer/docs.test.ts
@@ -6,12 +6,11 @@ const { keyboard } = page;
 
 // Helper functions used in multiple tests
 const menuDisplayState = () => page.evaluate(() => document.querySelector<HTMLDivElement>('#menu').style.display);
-const currentFocusId = () => page.evaluate(() => document.activeElement.id);
+const currentFocusID = () => page.evaluate(() => document.activeElement.id);
 
 // Tests
 beforeEach(async () => {
 	await page.goto(`file://${path.join(__dirname, '..', '..', 'docs', 'build', 'index.html')}`);
-	await page.waitFor('#menubutton');
 });
 
 it('has the correct page title', async () => {
@@ -22,7 +21,7 @@ it('focuses on the first menu item when the enter key is pressed', async () => {
 	await page.focus('#menubutton');
 	await keyboard.down('Enter');
 
-	expect(await currentFocusId()).toBe('menuitem1');
+	expect(await currentFocusID()).toBe('menuitem1');
 });
 
 it('hides the menu by default and make it visible if it is open', async () => {
@@ -40,7 +39,7 @@ it('hides the menu if the escape key is pressed, and focuses on the menu button'
 
 	await keyboard.down('Escape');
 	expect(await menuDisplayState()).toBe('none');
-	expect(await currentFocusId()).toBe('menubutton');
+	expect(await currentFocusID()).toBe('menubutton');
 });
 
 it('hides the menu if the tab key is pressed, and focuses on the next item in the tab order', async () => {
@@ -49,7 +48,7 @@ it('hides the menu if the tab key is pressed, and focuses on the next item in th
 
 	await keyboard.down('Tab');
 	expect(await menuDisplayState()).toBe('none');
-	expect(await currentFocusId()).toBe('secondbutton');
+	expect(await currentFocusID()).toBe('secondbutton');
 });
 
 it('hides the menu if the tab key is pressed while the shift key is held, and focuses on the previous item in the tab order', async () => {
@@ -59,5 +58,5 @@ it('hides the menu if the tab key is pressed while the shift key is held, and fo
 	await keyboard.down('Shift');
 	await keyboard.down('Tab');
 	expect(await menuDisplayState()).toBe('none');
-	expect(await currentFocusId()).toBe('menubutton');
+	expect(await currentFocusID()).toBe('menubutton');
 });
