3feeb38f2f8894f4ec13e1c5ebe82acc679f0301
Cory Harper <42228018+corymharper@users.noreply.github.com>
2019-12-18 07:49:42 -0600

    Add headless browser tests (#30)

--- test/puppeteer/docs.test.ts
+++ test/puppeteer/docs.test.ts
@@ -1,11 +1,49 @@
 // Imports
 import path from 'path';
 
+// Destructured constant for readability
+const { keyboard } = page;
+
+// Helper functions used in multiple tests
+const currentFocusID = () => page.evaluate(() => document.activeElement.id);
+
 // Tests
-beforeAll(async () => {
+beforeEach(async () => {
 	await page.goto(`file://${path.join(__dirname, '..', '..', 'docs', 'build', 'index.html')}`);
 });
 
-it('should have correct page title', async () => {
+it('has the correct page title', async () => {
 	await expect(page.title()).resolves.toMatch('React Accessible Dropdown Menu Hook');
+});
+
+it('focuses on the first menu item when the enter key is pressed', async () => {
+	await page.focus('#menubutton');
+	await keyboard.down('Enter');
+
+	expect(await currentFocusID()).toBe('menuitem1');
+});
+
+it('focuses on the menu button after pressing escape', async () => {
+	await page.focus('#menubutton');
+	await keyboard.down('Enter');
+	await keyboard.down('Escape');
+
+	expect(await currentFocusID()).toBe('menubutton');
+});
+
+it('focuses on the next item in the tab order after pressing tab', async () => {
+	await page.focus('#menubutton');
+	await keyboard.down('Enter');
+	await keyboard.down('Tab');
+
+	expect(await currentFocusID()).toBe('secondbutton');
+});
+
+it('focuses on the previous item in the tab order after pressing shift-tab', async () => {
+	await page.focus('#menubutton');
+	await keyboard.down('Enter');
+	await keyboard.down('Shift');
+	await keyboard.down('Tab');
+
+	expect(await currentFocusID()).toBe('menubutton');
 });
