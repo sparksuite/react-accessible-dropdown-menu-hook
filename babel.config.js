39db0214bf49934c42e2394b30bafc4f3b333202
Wes Cossick <wes@sparksuite.com>
2020-01-15 17:19:13 -0600

    Update packages and remove Babel plugins

--- babel.config.js
+++ babel.config.js
@@ -11,8 +11,4 @@ module.exports = {
 		'@babel/preset-typescript',
 		'@babel/preset-react',
 	],
-	plugins: [
-		'@babel/plugin-proposal-nullish-coalescing-operator', // TODO: Will become unnecessary soon: https://github.com/babel/babel/issues/10690
-		'@babel/plugin-proposal-optional-chaining', // TODO: Will become unnecessary soon: https://github.com/babel/babel/issues/10690
-	],
 };
