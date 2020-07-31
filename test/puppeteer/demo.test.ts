e648658e973f532fcbe426f2a9837eb300def2fd
Wes Cossick <wes@sparksuite.com>
2020-07-31 12:57:57 -0500

    Reset page for every test

--- test/puppeteer/demo.test.ts
+++ test/puppeteer/demo.test.ts
@@ -1,9 +1,6 @@
 // Imports
 import path from 'path';
 
-// Destructured constant for readability
-const { keyboard } = page;
-
 // Helper functions used in multiple tests
 const currentFocusID = () => page.evaluate(() => document.activeElement.id);
 const menuOpen = () => page.waitForSelector('#menu', { visible: true });
@@ -11,6 +8,8 @@ const menuClosed = () => page.waitForSelector('#menu', { hidden: true });
 
 // Tests
 beforeEach(async () => {
+	await jestPuppeteer.resetPage();
+
 	await page.goto(`file://${path.join(__dirname, '..', '..', 'demo', 'build', 'index.html')}`, {
 		waitUntil: 'load',
 	});
@@ -29,10 +28,10 @@ it('leaves focus on the button after clicking it', async () => {
 
 it('focuses on the menu button after pressing escape', async () => {
 	await page.focus('#menu-button');
-	await keyboard.down('Enter');
+	await page.keyboard.down('Enter');
 	await menuOpen();
 
-	await keyboard.down('Escape');
+	await page.keyboard.down('Escape');
 	await menuClosed();
 
 	expect(await currentFocusID()).toBe('menu-button');
@@ -40,10 +39,10 @@ it('focuses on the menu button after pressing escape', async () => {
 
 it('focuses on the next item in the tab order after pressing tab', async () => {
 	await page.focus('#menu-button');
-	await keyboard.down('Enter');
+	await page.keyboard.down('Enter');
 	await menuOpen();
 
-	await keyboard.down('Tab');
+	await page.keyboard.down('Tab');
 	await menuClosed();
 
 	expect(await currentFocusID()).toBe('first-footer-link');
@@ -51,11 +50,11 @@ it('focuses on the next item in the tab order after pressing tab', async () => {
 
 it('focuses on the previous item in the tab order after pressing shift-tab', async () => {
 	await page.focus('#menu-button');
-	await keyboard.down('Enter');
+	await page.keyboard.down('Enter');
 	await menuOpen();
 
-	await keyboard.down('Shift');
-	await keyboard.down('Tab');
+	await page.keyboard.down('Shift');
+	await page.keyboard.down('Tab');
 	await menuClosed();
 
 	expect(await currentFocusID()).toBe('menu-button');
@@ -63,7 +62,7 @@ it('focuses on the previous item in the tab order after pressing shift-tab', asy
 
 it('closes the menu if you click outside of it', async () => {
 	await page.focus('#menu-button');
-	await keyboard.down('Enter');
+	await page.keyboard.down('Enter');
 	await menuOpen();
 
 	await page.click('h1');
@@ -74,7 +73,7 @@ it('closes the menu if you click outside of it', async () => {
 
 it('leaves the menu open if you click inside of it', async () => {
 	await page.focus('#menu-button');
-	await keyboard.down('Enter');
+	await page.keyboard.down('Enter');
 	await menuOpen();
 
 	await page.click('#menu-item-1');
@@ -92,7 +91,7 @@ it('reroutes enter presses on menu items as clicks', async () => {
 	let alertAppeared = false;
 
 	await page.focus('#menu-button');
-	await keyboard.down('Enter');
+	await page.keyboard.down('Enter');
 	await menuOpen();
 
 	// eslint-disable-next-line @typescript-eslint/no-misused-promises
@@ -102,7 +101,7 @@ it('reroutes enter presses on menu items as clicks', async () => {
 	});
 
 	await page.focus('#menu-item-3');
-	await keyboard.down('Enter');
+	await page.keyboard.down('Enter');
 
 	expect(alertAppeared).toBe(true);
 });
