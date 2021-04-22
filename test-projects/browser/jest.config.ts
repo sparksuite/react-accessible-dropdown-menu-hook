8c3771db5f68a48b4f4000554bf798b89a181673
Wes Cossick <wes@sparksuite.com>
2021-04-22 12:35:55 -0500

    Add another comment

--- test-projects/browser/jest.config.ts
+++ test-projects/browser/jest.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from '@jest/types';
 // @ts-ignore: Missing types
 import presetTSJest from 'ts-jest/jest-preset';
+// @ts-ignore: Missing types
 import presetJestPuppeteer from 'jest-puppeteer/jest-preset.js';
 import merge from 'merge';
 
