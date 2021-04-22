7d7b6dae0e0a0edca446e38281f34f5b9ebd0392
Wes Cossick <wes@sparksuite.com>
2021-04-22 12:19:16 -0500

    Tweak Jest config file

--- test-projects/browser/jest.config.ts
+++ test-projects/browser/jest.config.ts
@@ -1,7 +1,6 @@
 import type { Config } from '@jest/types';
-// @ts-expect-error: Missing types
 import presetTSJest from 'ts-jest/jest-preset';
-import presetJestPuppeteer from 'jest-puppeteer/jest-preset.json';
+import presetJestPuppeteer from 'jest-puppeteer/jest-preset.js';
 import merge from 'merge';
 
 const config: Config.InitialOptions = merge.recursive(presetTSJest, presetJestPuppeteer, {
