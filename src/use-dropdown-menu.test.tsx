e81b444a603e1ec53c3779ea6f967ff36e2d989f
Cory Harper <zepher2211@gmail.com>
2021-05-19 15:23:45 -0500

    Added unit test for space functionality

--- src/use-dropdown-menu.test.tsx
+++ src/use-dropdown-menu.test.tsx
@@ -464,3 +464,21 @@ it('Activates the click handler of a menu item after pressing enter while focuse
 
 	expect(console.log).toHaveBeenCalledWith('Item one clicked');
 });
+
+it('Activates the click handler of a menu item after pressing space while focused on it', () => {
+	render(<TestComponent />);
+
+	jest.spyOn(console, 'log');
+
+	userEvent.tab();
+
+	userEvent.type(screen.getByText('Primary'), '{enter}', {
+		skipClick: true,
+	});
+
+	userEvent.type(screen.getByText('Item 1'), '{space}', {
+		skipClick: true,
+	});
+
+	expect(console.log).toHaveBeenCalledWith('Item one clicked');
+});
